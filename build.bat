c309deb9ac9510e7a0d85d7a22d0ed0e16e0676c
Glitchgod <84155577+glitchgod0@users.noreply.github.com>
2026-07-21 10:24:27 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -7,4 +7,7 @@ move _qb.pak.xen out\xbox\data\pak\qb.pak.xen
 move _qb.pab.xen out\xbox\data\pak\qb.pab.xen
 
 ECHO "Just for testing..."
+
+copy "out\xbox\data\pak\qb.pak.xen" "L:\Beta\Protos\GH\Guitar Hero III - Legends of Rock (Jul 6, 2007 prototype)\GH3 DEMO BUILD\DATA\pak\qb.pak.xen"
+copy "out\xbox\data\pak\qb.pab.xen" "L:\Beta\Protos\GH\Guitar Hero III - Legends of Rock (Jul 6, 2007 prototype)\GH3 DEMO BUILD\DATA\pak\qb.pab.xen"
 D:\Xenia\xenia_canary.exe "L:\Beta\Protos\GH\Guitar Hero III - Legends of Rock (Jul 6, 2007 prototype)\GH3 DEMO BUILD\GH3.xex"
