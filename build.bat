21adede771d9f4611948e87f8c547751aea06313
Glitchgod <84155577+glitchgod0@users.noreply.github.com>
2026-07-23 00:40:04 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,4 +5,4 @@ mkdir out\xbox\data\pak
 
 move _qb.pak.xen out\xbox\DATA\pak\qb.pak.xen
 move _qb.pab.xen out\xbox\DATA\pak\qb.pab.xen
-copy "dependencies\files\xbox\DATA\COMPRESSED\compress.toc.xen" "out\xbox\DATA\COMPRESSED\compress.toc.xen" 
+xcopy "dependencies\files\xbox\DATA\COMPRESSED\compress.toc.xen" "out\xbox\DATA\COMPRESSED\" /I /Y
